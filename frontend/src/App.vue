d3405efc08dc8b457f0f93ea593c645c01900a06
Zafer Gürkan <zafergurkan@outlook.com>
2021-02-27 18:36:31 +0300

    start

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="(Zafer Bilal | Enes Hamza) Gürkan "/>
   </div>
 </template>
 
